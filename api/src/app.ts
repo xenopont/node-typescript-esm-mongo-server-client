743f2955eb149a788c12dd59e36cbeaf8d0c19ff
John Smith <smith404@live.com>
2024-04-20 00:35:03 +0200

    logger added (pino)

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import { logger } from "./utils/logger.js";
+
+const main = async (): Promise<void> => {
+  throw new Error("Method not implemented.");
+};
+
+main().catch((error: Error) => {
+  logger.error(error);
+});
